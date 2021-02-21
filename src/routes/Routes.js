1582fabfb17d014afe864f8b4e14fed08baf943a
Mauricio <maubot01@gmail.com>
2021-02-21 17:04:43 -0400

    Artists route

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -5,6 +5,7 @@ import { Switch, Route } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import Settings from "../pages/Settings/Settings";
 import Artist from "../pages/Artist/Artist";
+import Artists from "../pages/Artists/Artists";
 
 const Routes = (props) => {
   const { user, setReloadApp } = props;
@@ -15,6 +16,9 @@ const Routes = (props) => {
       </Route>
       <Route path="/artist/:id" exact>
         <Artist />
+      </Route>
+      <Route path="/artists" exact>
+        <Artists />
       </Route>
       <Route path="/settings" exact>
         <Settings user={user} setReloadApp={setReloadApp} />
