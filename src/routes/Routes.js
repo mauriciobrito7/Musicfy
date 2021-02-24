a35bc0b5a27a79f2d70ad108996947e60dcd15e5
Mauricio <maubot01@gmail.com>
2021-02-24 13:38:21 -0400

    lazy imports

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,37 +1,39 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Switch, Route } from "react-router-dom";
+import { Loader } from "semantic-ui-react";
 
-// Pages
-import Home from "../pages/Home/Home";
-import Settings from "../pages/Settings/Settings";
-import Artist from "../pages/Artist/Artist";
-import Artists from "../pages/Artists/Artists";
-import Albums from "../pages/Albums/Albums";
-import Album from "../pages/Album/Album";
+const Home = React.lazy(() => import("../pages/Home/Home"));
+const Settings = React.lazy(() => import("../pages/Settings/Settings"));
+const Artist = React.lazy(() => import("../pages/Artist/Artist"));
+const Artists = React.lazy(() => import("../pages/Artists/Artists"));
+const Albums = React.lazy(() => import("../pages/Albums/Albums"));
+const Album = React.lazy(() => import("../pages/Album/Album"));
 
 const Routes = (props) => {
   const { user, setReloadApp } = props;
   return (
-    <Switch>
-      <Route path="/" exact>
-        <Home />
-      </Route>
-      <Route path="/artist/:id" exact>
-        <Artist />
-      </Route>
-      <Route path="/artists" exact>
-        <Artists />
-      </Route>
-      <Route path="/albums" exact>
-        <Albums />
-      </Route>
-      <Route path="/album/:id" exact>
-        <Album />
-      </Route>
-      <Route path="/settings" exact>
-        <Settings user={user} setReloadApp={setReloadApp} />
-      </Route>
-    </Switch>
+    <Suspense fallback={<Loader>Loading...</Loader>}>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/artist/:id" exact>
+          <Artist />
+        </Route>
+        <Route path="/artists" exact>
+          <Artists />
+        </Route>
+        <Route path="/albums" exact>
+          <Albums />
+        </Route>
+        <Route path="/album/:id" exact>
+          <Album />
+        </Route>
+        <Route path="/settings" exact>
+          <Settings user={user} setReloadApp={setReloadApp} />
+        </Route>
+      </Switch>
+    </Suspense>
   );
 };
 
