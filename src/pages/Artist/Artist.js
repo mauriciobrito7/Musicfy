889a052e953826b1c28191e07d6186e0ba51c3e3
Mauricio <maubot01@gmail.com>
2021-02-25 09:15:28 -0400

    show albums of the artist

--- src/pages/Artist/Artist.js
+++ src/pages/Artist/Artist.js
@@ -3,19 +3,22 @@ import "./Artist.scss";
 import { withRouter } from "react-router-dom";
 import firebase from "../../utils/firebase";
 import BannerArtist from "../../components/Artists/BannerArtist/BannerArtist";
+import BasicSliderItems from "../../components/Sliders/BasicSliderItems/BasicSliderItems";
 
 const Artist = (props) => {
   const { params } = props.match;
   const [artist, setArtist] = useState(null);
+  const [albums, setAlbums] = useState([]);
 
   useEffect(() => {
     firebase
       .firestore()
-      .collection("/artists")
+      .collection("artists")
       .doc(params?.id)
       .get()
       .then((doc) => {
         const data = doc.data();
+        data.id = doc.id;
         setArtist(data);
       })
       .catch((err) => {
@@ -23,8 +26,37 @@ const Artist = (props) => {
       });
   }, [params]);
 
+  useEffect(() => {
+    console.log(artist);
+    if (artist) {
+      firebase
+        .firestore()
+        .collection("albums")
+        .where("artist", "==", artist.id)
+        .get()
+        .then((albums) => {
+          const arrayAlbums = albums?.docs.map((album) => {
+            const data = album.data();
+            data.id = album.id;
+            return data;
+          });
+          setAlbums(arrayAlbums);
+        });
+    }
+  }, [artist]);
+
   return (
-    <div className="artist">{artist && <BannerArtist artist={artist} />}</div>
+    <div className="artist">
+      {artist && <BannerArtist artist={artist} />}
+      <div className="artist__content">
+        <BasicSliderItems
+          title="Álbumes"
+          data={albums}
+          folderImage="album"
+          urlName="album"
+        />
+      </div>
+    </div>
   );
 };
 
